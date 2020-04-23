471915f4cd607a912d5063c571a19d6b6390017f
yusuke-hub <yk1210140408@gmail.com>
2020-04-23 15:37:46 +0900

    6 calculation

--- Google ドライブ/java-practice/Main.java
+++ Google ドライブ/java-practice/Main.java
@@ -1,24 +1,19 @@
 public class Main {
   public static void main(String[] args) {
-    // 文字列
-    String msg;
-    // 文字
-    char a = 'a';
-    // 整数 byte short int long(末尾にlかLをつける！)
-    int x = 10;
-    long y = 5555555555L;
-    // 浮動小数点数 float(末尾にfかFをつける！) double(より大きな桁数を扱う事ができる)
-    double d = 23423.344;
-    float f = 32.33F;
-    // 論理値
-    boolean flag = true;
-    // 特殊記号
-    // \n 改行
-    // \t タブ
-    
-    // 変数
-    // 型 変数名
-    msg = "Hello World again!";
-    System.out.println(msg);
+
+    // 演算
+    // + - * / %
+    // ++ --
+    int i;
+    i = 10 / 3;
+    System.out.println(i);
+    i = 10 % 3;
+    System.out.println(i);
+    int x = 5;
+    x++;
+    System.out.println(x);
+    x--;
+    System.out.println(x);
+
   }
 }
