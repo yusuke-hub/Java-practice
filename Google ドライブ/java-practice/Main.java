b9250ec246b9965ed8923ec4b680f6e48cb528f5
yusuke-hub <yk1210140408@gmail.com>
2020-04-23 15:23:33 +0900

    4-5 Variable / Data Type

--- Google ドライブ/java-practice/Main.java
+++ Google ドライブ/java-practice/Main.java
@@ -1,5 +1,24 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("テストです");
+    // 文字列
+    String msg;
+    // 文字
+    char a = 'a';
+    // 整数 byte short int long(末尾にlかLをつける！)
+    int x = 10;
+    long y = 5555555555L;
+    // 浮動小数点数 float(末尾にfかFをつける！) double(より大きな桁数を扱う事ができる)
+    double d = 23423.344;
+    float f = 32.33F;
+    // 論理値
+    boolean flag = true;
+    // 特殊記号
+    // \n 改行
+    // \t タブ
+    
+    // 変数
+    // 型 変数名
+    msg = "Hello World again!";
+    System.out.println(msg);
   }
 }
