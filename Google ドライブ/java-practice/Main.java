23184b708b7868fcc7031d3056206d4f095f00dc
yusuke-hub <yk1210140408@gmail.com>
2020-04-23 15:48:37 +0900

    7 Cast

--- Google ドライブ/java-practice/Main.java
+++ Google ドライブ/java-practice/Main.java
@@ -1,19 +1,17 @@
 public class Main {
-  public static void main(String[] args) {
+  public static void main(final String[] args) {
+    // キャスト
+    // 文頭で変換したいデータ型をカッコで囲む
+    // double d = 52343.231;
+    // int i = (int)d;
+    // System.out.println(i);
 
-    // 演算
-    // + - * / %
-    // ++ --
-    int i;
-    i = 10 / 3;
-    System.out.println(i);
-    i = 10 % 3;
-    System.out.println(i);
-    int x = 5;
-    x++;
-    System.out.println(x);
-    x--;
-    System.out.println(x);
+    int i = 10;
+    // 整数型iを4で割ると、計算自体は2.5だが整数型なので小数点以下は切り捨てられ2になる
+    // それがdouble型に代入されるので、答えは2.0
+    // double d = i / 4;
+    double d = (double) i / 4;
+    System.out.println(d);
 
   }
 }
