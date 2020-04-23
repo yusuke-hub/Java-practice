dd2bc5c705d0e988e5fdfe6c3b9165e2e2ecedfe
yusuke-hub <yk1210140408@gmail.com>
2020-04-23 15:56:06 +0900

    8 Conditional Branch

--- Google ドライブ/java-practice/Main.java
+++ Google ドライブ/java-practice/Main.java
@@ -1,17 +1,17 @@
 public class Main {
   public static void main(final String[] args) {
-    // キャスト
-    // 文頭で変換したいデータ型をカッコで囲む
-    // double d = 52343.231;
-    // int i = (int)d;
-    // System.out.println(i);
-
-    int i = 10;
-    // 整数型iを4で割ると、計算自体は2.5だが整数型なので小数点以下は切り捨てられ2になる
-    // それがdouble型に代入されるので、答えは2.0
-    // double d = i / 4;
-    double d = (double) i / 4;
-    System.out.println(d);
-
+    // if
+    // > >= < <= == !=
+    // && ||
+    int score = 45;
+    // if (score > 80) {
+    //   System.out.println("Great!");
+    // } else if (score > 60) {
+    //   System.out.println("Good!");
+    // } else {
+    //   System.out.println("so so...!");
+    // }
+    String msg = score > 80 ? "Great!" : "so so ...!";
+      System.out.println(msg);
   }
 }
