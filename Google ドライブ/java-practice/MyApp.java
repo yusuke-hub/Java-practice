9dc95184c95962d014a5d39e8275dda1009be608
yusuke-hub <yk1210140408@gmail.com>
2020-04-24 11:33:20 +0900

    19 Constructor

--- Google ドライブ/java-practice/MyApp.java
+++ Google ドライブ/java-practice/MyApp.java
@@ -1,19 +1,31 @@
-// class名の先頭は大文字
 class User {
-  String name = "Me!";
+  String name;
+
+  // constructor
+  User(String name) {
+    this.name = name;
+  }
+
+  // this()
+
+  User() {
+    // this.name = "Me!";
+    this("Me!");
+  }
 
   void sayHi() {
-    System.out.println("Hi!");
+    System.out.println("hi! " + this.name);
   }
 }
 
 public class MyApp {
 
-  public static void main(final String[] args) {
-
-    // Class
-    User tom = new User();
+  public static void main(String[] args) {
+    User tom;
+    // tom = new User("Tom");
+    tom = new User();
     System.out.println(tom.name);
     tom.sayHi();
   }
+
 }
