c78763bc7e6f692464b8abc03c1b3f1c84794bf9
yusuke-hub <yk1210140408@gmail.com>
2020-04-24 11:22:59 +0900

    18 Class

--- Google ドライブ/java-practice/MyApp.java
+++ Google ドライブ/java-practice/MyApp.java
@@ -1,18 +1,19 @@
+// class名の先頭は大文字
+class User {
+  String name = "Me!";
+
+  void sayHi() {
+    System.out.println("Hi!");
+  }
+}
+
 public class MyApp {
 
-  // sayHiメソッド内の変数xはmainメソッド内では使用できない
-  public static void sayHi(String name) {
-    // int x = 10;
-    System.out.println("Hi!" + name);
-  }
-
-  // Overload
-  // 引数なしのsayHiを定義する
-  public static void sayHi() {
-    System.out.println("Hi! Nobody!");
-  }
-
   public static void main(final String[] args) {
-    sayHi();
+
+    // Class
+    User tom = new User();
+    System.out.println(tom.name);
+    tom.sayHi();
   }
 }
