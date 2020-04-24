fa56fa6ce181d5c9240582ba5ca229c4f656d074
yusuke-hub <yk1210140408@gmail.com>
2020-04-24 10:04:12 +0900

    13 Operate the element of Array

--- Google ドライブ/java-practice/Main.java
+++ Google ドライブ/java-practice/Main.java
@@ -2,30 +2,15 @@ public class Main {
   public static void main(final String[] args) {
 
     // 配列
-    // sales1, sales2...
-    // sales
+    final int[] sales = { 700, 400, 500 };
 
-    // データ型[]
-    // int[] sales;
+    // for (int i = 0; i < 3; i++) {
+    // System.out.println(sales[i]);
+    // }
 
-    // 配列を宣言したらデータ領域を確保するために,newを行う
-    // sales = new int[3];
-
-    // 1つずつ値を入れる方法
-      // 配列の要素は0から始まる
-      // 配列[添え字(index)]
-        // sales[0] = 100;
-        // sales[1] = 200;
-        // sales[2] = 300;
-
-    // まとめて値を入れる方法
-      // sales = new int[3] { 100, 200, 300 };
-        int[] sales = { 100, 200, 300 };
-
-        System.out.println(sales[1]);
-
-        sales[1] = 1000;
-        System.out.println(sales[1]);
-
+    // 配列salesの値を変数saleに順番に入れていき、その個数分だけ処理を実行する
+    for (int sale : sales) {
+      System.out.println(sale);
+    }
   }
 }
