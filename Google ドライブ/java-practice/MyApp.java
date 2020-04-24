2dfbd7d6c9ff8f23318a93dd0d82dedf21f0704d
yusuke-hub <yk1210140408@gmail.com>
2020-04-24 17:02:33 +0900

    26 Initializer

--- Google ドライブ/java-practice/MyApp.java
+++ Google ドライブ/java-practice/MyApp.java
@@ -5,10 +5,24 @@ class User {
   private String name;
   private static int count = 0;
 
+  // static {}
+  // クラスを使う前に、行いたい初期化処理を記述する
+  static {
+    User.count = 0;
+    System.out.println("Static initializer");
+  }
+
+  // インスタンス・イニシャライザ
+  // インスタンスを初期化する際に行う処理を書く
+  {
+    System.out.println("Instance initializer");
+  }
+
   public User(String name, int score) {
     this.name = name;
     // staticを定義したクラス変数は先頭にクラス名をつける事でアクセスできる
     User.count++;
+    System.out.println("Constructor");
   }
 
   public static void getInfo() {
