ead0269508dd4019a94dafc668599aae18dab527
yusuke-hub <yk1210140408@gmail.com>
2020-04-24 09:55:53 +0900

    12 Array

--- Google ドライブ/java-practice/Main.java
+++ Google ドライブ/java-practice/Main.java
@@ -1,19 +1,31 @@
 public class Main {
   public static void main(final String[] args) {
 
-    // for
-    // ある処理を指定した回数繰り返す
-    // 0 - 9
-    // break
-    // ループ処理を完全に停止する
-    // continue
-    // 指定した条件のみ、処理を飛ばして、処理を全て実行する
-    for (int i = 0; i < 10; i++) {
-      if (i == 5) {
-        // break;
-        continue;
-      }
-      System.out.println(i);
-    }
+    // 配列
+    // sales1, sales2...
+    // sales
+
+    // データ型[]
+    // int[] sales;
+
+    // 配列を宣言したらデータ領域を確保するために,newを行う
+    // sales = new int[3];
+
+    // 1つずつ値を入れる方法
+      // 配列の要素は0から始まる
+      // 配列[添え字(index)]
+        // sales[0] = 100;
+        // sales[1] = 200;
+        // sales[2] = 300;
+
+    // まとめて値を入れる方法
+      // sales = new int[3] { 100, 200, 300 };
+        int[] sales = { 100, 200, 300 };
+
+        System.out.println(sales[1]);
+
+        sales[1] = 1000;
+        System.out.println(sales[1]);
+
   }
 }
