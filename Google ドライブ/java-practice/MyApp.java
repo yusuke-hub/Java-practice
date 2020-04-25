196148b9472703a232239938623be95c8a731983
yusuke-hub <yk1210140408@gmail.com>
2020-04-25 11:21:10 +0900

    30 Enum

--- Google ドライブ/java-practice/MyApp.java
+++ Google ドライブ/java-practice/MyApp.java
@@ -1,39 +1,24 @@
-// interface 
-// クラスの仲間であり、抽象クラスの親戚みたいなもの
-// implementsする複数の小クラスたちに共通のメソッド群を実装するよう強制できる
-// あるクラスがインターフェースを実装していれば、少なくともそのインターフェースが定めたメソッドは持っている事が保証される
-
-// interfaceに宣言されたメソッドは自動的にpublicかつabstractになる
-interface Printable {
-
-  // interfaceは本来フィールドを持たないが、
-  // public static finalが着いたフィールドだけは宣言が許される
-  // 無ければ自動補完されるので、省略可
-  double VERSION = 1.2;
-
-  // 抽象メソッド
-  void print();
-
-  // defaultメソッド
-  public default void getInfo() {
-    System.out.println("I/F ver. " + Printable.VERSION);
-  }
-  // staticメソッド
-}
-
-class User implements Printable {
-  @Override
-  public void print() {
-    System.out.println("Now printing user profile...");
-  }
+// 列挙型
+enum Result {
+  SUCCESS, ERROR,
 }
 
 public class MyApp {
-
   public static void main(String[] args) {
-    User tom = new User();
-    tom.print();
-    tom.getInfo();
-  }
+    Result res;
 
+    res = Result.SUCCESS;
+
+    switch (res) {
+      case SUCCESS:
+        System.out.println("OK!");
+        System.out.println(res.ordinal());
+        break;
+      case ERROR:
+        System.out.println("NG!");
+        System.out.println(res.ordinal());
+        break;
+
+    }
+  }
 }
