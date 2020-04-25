585de197e481e241dd446a8aad14324cdad0ef79
yusuke-hub <yk1210140408@gmail.com>
2020-04-25 19:59:06 +0900

    31 Exception

--- Google ドライブ/java-practice/MyApp.java
+++ Google ドライブ/java-practice/MyApp.java
@@ -1,24 +1,31 @@
-// 列挙型
-enum Result {
-  SUCCESS, ERROR,
+// 例外
+
+class MyException extends Exception {
+  public MyException(String s) {
+    super(s);
+  }
 }
 
 public class MyApp {
-  public static void main(String[] args) {
-    Result res;
 
-    res = Result.SUCCESS;
-
-    switch (res) {
-      case SUCCESS:
-        System.out.println("OK!");
-        System.out.println(res.ordinal());
-        break;
-      case ERROR:
-        System.out.println("NG!");
-        System.out.println(res.ordinal());
-        break;
-
+  public static void div(int a, int b) {
+    try {
+      if (b < 0) {
+        throw new MyException("not minus!");
+      }
+      System.out.println(a / b);
+    } catch (ArithmeticException e) {
+      System.err.println(e.getMessage());
+    } catch (MyException e) {
+      System.err.println(e.getMessage());
+    } finally {
+      System.out.println(" -- end -- ");
     }
   }
+
+  public static void main(String[] args) {
+    div(3, 0);
+    div(5, -2);
+  }
+
 }
