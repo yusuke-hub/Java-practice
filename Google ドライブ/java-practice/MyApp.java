da1f316ec7bf4593f430c488ff33cb29b2960d81
yusuke-hub <yk1210140408@gmail.com>
2020-05-09 14:57:52 +0900

    39 ArrayList

--- Google ドライブ/java-practice/MyApp.java
+++ Google ドライブ/java-practice/MyApp.java
@@ -1,19 +1,30 @@
-
-// Randomクラスをjavaのutilパッケージからimport
-import java.util.Random;
+import java.util.*;
 
 public class MyApp {
+  // ArrayList
+  // リストは配列とよく似ており、値の集合を扱うために使用する
+  // リストは要素が追加された順番を保持し、重複した要素もそのまま保持できる
+
   public static void main(String[] args) {
-    double d = 35.121;
-    System.out.println(Math.ceil(d)); // 切り上げ
-    System.out.println(Math.floor(d)); // 切り捨て
-    System.out.println(Math.round(d)); // 四捨五入
-    System.out.println(Math.PI); // 円周率
+    // リストの要素は参照型である必要があるため、intのような基本データ型を要素に持つリストは作成できない。
+    // 基本型を扱うリストを作成したい場合は、下記のようにラッパークラスを用いて定義する。
+    List<Integer> sales = new ArrayList<>();
 
-    Random r = new Random();
-    System.out.println(r.nextDouble()); // 0 - 1
-    System.out.println(r.nextInt(100)); // 0 - 100
-    System.out.println(r.nextBoolean()); // 真偽値
+    sales.add(10);
+    sales.add(20);
+    sales.add(30);
 
+    for (int i = 0; i < sales.size(); i++) {
+      System.out.println(sales.get(i));
+    }
+
+    // 0番目の要素を100に置き換える
+    sales.set(0, 100);
+    // 2番目の要素を削除する
+    sales.remove(2);
+
+    for (Integer sale : sales) {
+      System.out.println(sale);
+    }
   }
 }
