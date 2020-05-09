2c3d86affac383fc22ad501308b7763c0f2fdb40
yusuke-hub <yk1210140408@gmail.com>
2020-05-09 15:08:19 +0900

    40 HashSet

--- Google ドライブ/java-practice/MyApp.java
+++ Google ドライブ/java-practice/MyApp.java
@@ -6,22 +6,28 @@ public class MyApp {
   // リストは要素が追加された順番を保持し、重複した要素もそのまま保持できる
 
   public static void main(String[] args) {
-    // リストの要素は参照型である必要があるため、intのような基本データ型を要素に持つリストは作成できない。
-    // 基本型を扱うリストを作成したい場合は、下記のようにラッパークラスを用いて定義する。
-    List<Integer> sales = new ArrayList<>();
+    // HashSet
+    // 重複して同じ値を入れる事はできない
+    // データを保持する順番が定まらない
 
-    sales.add(10);
+    // TreeSet
+    // 値順にソートされる
+    // LinkedHachSet
+    // 追加された順番を保持する
+    
+    // HashSet<Integer> sales = new HashSet<>();
+    TreeSet<Integer> sales = new TreeSet<>();
+    sales.add(40);
     sales.add(20);
-    sales.add(30);
+    sales.add(80);
+    sales.add(10);
 
-    for (int i = 0; i < sales.size(); i++) {
-      System.out.println(sales.get(i));
+    System.out.println(sales.size());
+    for (Integer sale : sales) {
+      System.out.println(sale);
     }
 
-    // 0番目の要素を100に置き換える
-    sales.set(0, 100);
-    // 2番目の要素を削除する
-    sales.remove(2);
+    sales.remove(10);
 
     for (Integer sale : sales) {
       System.out.println(sale);
