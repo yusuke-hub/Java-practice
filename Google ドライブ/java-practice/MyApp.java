0e76c985f10e8d7def0431b2bf750ac2494f72b1
yusuke-hub <yk1210140408@gmail.com>
2020-05-09 09:11:14 +0900

    32 Wrapper Class

--- Google ドライブ/java-practice/MyApp.java
+++ Google ドライブ/java-practice/MyApp.java
@@ -1,31 +1,19 @@
-// 例外
-
-class MyException extends Exception {
-  public MyException(String s) {
-    super(s);
-  }
-}
+/*
+Wrapper Class
+int -> Integer
+double -> Double
+*/
 
 public class MyApp {
 
-  public static void div(int a, int b) {
-    try {
-      if (b < 0) {
-        throw new MyException("not minus!");
-      }
-      System.out.println(a / b);
-    } catch (ArithmeticException e) {
-      System.err.println(e.getMessage());
-    } catch (MyException e) {
-      System.err.println(e.getMessage());
-    } finally {
-      System.out.println(" -- end -- ");
-    }
-  }
-
   public static void main(String[] args) {
-    div(3, 0);
-    div(5, -2);
+    // Integer i = new Integer(32);
+    // int n = i.intValue();
+
+    Integer i = 32; // auto boxing
+    i = null;
+    int n = i; // auto unboxing
+
   }
 
 }
