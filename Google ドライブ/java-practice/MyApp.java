0b4d2997518f6c13e38f2c1ac391916579b9d34c
yusuke-hub <yk1210140408@gmail.com>
2020-05-09 11:01:26 +0900

    37 printf

--- Google ドライブ/java-practice/MyApp.java
+++ Google ドライブ/java-practice/MyApp.java
@@ -1,33 +1,14 @@
 public class MyApp {
   public static void main(String[] args) {
-    String s = "abcdef";
-    System.out.println(s.length());
-    // substring(i1, i2)
-    // i1番目からi2番目の手前までの文字列を出力する
-    System.out.println(s.substring(1, 4));
-    // replaceALL("s1", "s2")
-    // s1をs2に置換する
-    System.out.println(s.replaceAll("cd", "CD"));
 
-    String s1 = "ab";
-    String s2 = "ab";
+    int score = 50;
+    double height = 169.5;
+    String name = "Kawabata";
 
-    // 文字列型においては、既に宣言されている変数と同じ文字列で
-    // 新たに変数が宣言された場合、既に宣言されて居る変数と同じメモリ領域が使われる
-    if (s1.equals(s2)) {
-      System.out.println("same!");
-    }
+    System.out.printf("name: %s, score: %d, height: %f\n", name, score, height);
+    System.out.printf("name: %-10s, score: %-10d, height: %5.3f\n", name, score, height);
 
-    if (s1 == s2) {
-      System.out.println("same!same!");
-    }
-
-    // 新しいメモリ領域を確保したい場合は、以下のように明示的にインスタンス化する
-    String ss1 = new String("ab");
-    String ss2 = new String("ab");
-
-    if (ss1 == ss2) {
-      System.out.println("same!same!same!");
-    }
+    String s = String.format("name: %10s, score: %10d, height: %5.2f\n", name, score, height);
+    System.out.println(s);
   }
 }
