ba5106e21f6013c01ca2f83aaa1c2e5f12e657c4
yusuke-hub <yk1210140408@gmail.com>
2020-05-09 09:11:38 +0900

    33 Generics

--- Google ドライブ/java-practice/MyApp.java
+++ Google ドライブ/java-practice/MyApp.java
@@ -1,19 +1,22 @@
-/*
-Wrapper Class
-int -> Integer
-double -> Double
-*/
+// generics
+// 汎用化されたデータ型でクラスやインターフェースを作ることができる
+
+// <>という記号をつけて、汎用化する方をどう表現するか指定する
+// Typeと言う意味で大文字のTが使われることが多い
+class MyData<T> {
+  public void getThree(T x) {
+    System.out.println(x);
+    System.out.println(x);
+    System.out.println(x);
+  }
+}
 
 public class MyApp {
-
   public static void main(String[] args) {
-    // Integer i = new Integer(32);
-    // int n = i.intValue();
-
-    Integer i = 32; // auto boxing
-    i = null;
-    int n = i; // auto unboxing
-
+    // <>内に入るデータ型は参照型のみ
+    MyData<Integer> i = new MyData<>();
+    i.getThree(55);
+    MyData<String> s = new MyData<>();
+    s.getThree("Matui");
   }
-
 }
