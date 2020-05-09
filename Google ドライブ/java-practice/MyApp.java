5ab05a8f2340918887e9f48cf8844f599a0b16db
yusuke-hub <yk1210140408@gmail.com>
2020-05-09 18:44:34 +0900

    43 LocalDateTime

--- Google ドライブ/java-practice/MyApp.java
+++ Google ドライブ/java-practice/MyApp.java
@@ -1,28 +1,23 @@
 import java.util.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class MyApp {
   public static void main(String[] args) {
+    // LocalDateTime
+    // タイムゾーンを考慮しないクラス
+    LocalDateTime d = LocalDateTime.now();
+    // LocalDateTime d = LocalDateTime.of(2016, 1, 1, 10, 10, 10);
+    // LocalDateTime d = LocalDateTime.parse("2016-01-01T10:10:10");
 
-    // Stream
-    // 複数の値を順番に処理していくための仕組み。
-    // ArrayListなどの集合データと合わせて、使われる
+    System.out.println(d.getYear());
+    System.out.println(d.getMonth());
+    System.out.println(d.getMonth().getValue());
+    System.out.println(d.plusMonths(2).minusDays(3));
 
-    // 宣言と同時に値を入れ込んでいく方法↓
-    // Arrays.asList()で配列を作り、値を入れ込んでいく
-    List<Integer> sales = new ArrayList<>(Arrays.asList(12, 30, 22, 4, 9));
+    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy!MM!dd!");
+    System.out.println(d.format(dtf));
 
-    sales.stream()
-        // 中間処理
-        // 指定した条件が真の値だけを抽出できる
-        .filter(e -> e % 3 == 0).map(e -> "(" + e
-            + ")")
-
-        // 終端処理
-        // .forEach
-        // 抽出した値に対して、処理を実行する
-        // メソッド参照という仕組みを用いて、メソッド自体を.forEach()に渡すことができる.
-        
-        .forEach(System.out::println);
   }
 
 }
