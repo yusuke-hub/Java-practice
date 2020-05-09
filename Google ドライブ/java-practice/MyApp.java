ec954e439becf2c6aa6d4b0393de5acd005e6f07
yusuke-hub <yk1210140408@gmail.com>
2020-05-09 11:10:55 +0900

    37 printf / format

--- Google ドライブ/java-practice/MyApp.java
+++ Google ドライブ/java-practice/MyApp.java
@@ -1,33 +1,20 @@
 public class MyApp {
   public static void main(String[] args) {
-    String s = "abcdef";
-    System.out.println(s.length());
-    // substring(i1, i2)
-    // i1番目からi2番目の手前までの文字列を出力する
-    System.out.println(s.substring(1, 4));
-    // replaceALL("s1", "s2")
-    // s1をs2に置換する
-    System.out.println(s.replaceAll("cd", "CD"));
 
-    String s1 = "ab";
-    String s2 = "ab";
+    int score = 50;
+    double height = 169.5;
+    String name = "Kawabata";
+    // printf
+    // 書式を指定して、出力できるメソッド
+    // format
+    // printfと似ているメソッド
 
-    // 文字列型においては、既に宣言されている変数と同じ文字列で
-    // 新たに変数が宣言された場合、既に宣言されて居る変数と同じメモリ領域が使われる
-    if (s1.equals(s2)) {
-      System.out.println("same!");
-    }
+    // 10:左寄せ -10:右寄せ
+    // %[整数の表示する桁数].[小数点以下の表示する桁数]f
+    System.out.printf("name: %s, score: %d, height: %f\n", name, score, height);
+    System.out.printf("name: %-10s, score: %-10d, height: %5.3f\n", name, score, height);
 
-    if (s1 == s2) {
-      System.out.println("same!same!");
-    }
-
-    // 新しいメモリ領域を確保したい場合は、以下のように明示的にインスタンス化する
-    String ss1 = new String("ab");
-    String ss2 = new String("ab");
-
-    if (ss1 == ss2) {
-      System.out.println("same!same!same!");
-    }
+    String s = String.format("name: %10s, score: %10d, height: %5.2f\n", name, score, height);
+    System.out.println(s);
   }
 }
