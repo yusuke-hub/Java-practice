d2429ffee9348fcd84ac297294c5e66d2b03d7e5
yusuke-hub <yk1210140408@gmail.com>
2020-05-15 10:38:11 +0900

    3/34 -37

--- Google ドライブ/java-practice/Operators/src/academy/learnprograming/Main.java
+++ Google ドライブ/java-practice/Operators/src/academy/learnprograming/Main.java
@@ -39,6 +39,76 @@ public class Main {
         result -= 2;
         System.out.println("13 - 2 = " + result);
 
+        // if - then
+        // 原則処理が１行の場合のみ
+        // 条件式の後に、セミコロンをつけてしまうと
+        // 条件式と処理が別々のコードと認識されてしまうので注意
+
+        boolean isAlien = false;
+        if(isAlien == false)
+            System.out.println("It is not an alien!");
+
+        // Code Block
+        // 処理を{}で囲む
+        // 実務においては、if - thenではなく、こちらが使われる
+        // なぜならif - thenで書くと、将来的に処理を追加する時
+        // やデバッグする時に面倒になるから
+        if (isAlien == false){
+            System.out.println("It is not an alien!");
+            System.out.println("And I am scared of aliens");
+        }
+
+        int topScore1 = 100;
+        if (topScore1 == 100) {
+            System.out.println("You got the high score!");
+        }
+        if (topScore1 != 100) {
+            System.out.println("You got the high score!");
+        }
+
+        int topScore2 = 80;
+        if (topScore2 < 100) {
+            System.out.println("You got the high score!");
+        }
+
+        int secondTopScore1 = 60;
+        if (topScore2 > secondTopScore1 && topScore2 < 100) {
+            System.out.println("Greater than second top score and less than 100");
+        }
+
+        // 条件式を並列させる場合は、他の人がぱっと見て
+        // コードの内容を理解できるように()で囲むなどの配慮を忘れない
+        int secondTopScore2 = 95;
+
+        // && means AND, & calls ampersand.
+        if ((topScore2 > secondTopScore2) && (topScore2 < 100)){
+            System.out.println("Either of both of the conditions are true");
+        }
+
+        // || means OR, | calls pipe.
+        if ((topScore2 > 90) || (secondTopScore2 <= 90)) {
+            System.out.println();
+        }
+
+        int newValue = 50;
+        // if (newValue = 50) { ← error
+        if (newValue == 50) {
+            System.out.println("This is true.");
+        }
+
+        boolean isCar = false;
+        // if (isCar == true) { ← error
+        // if (isCar = true) {
+
+
+        // if (isCar != true) {
+        // 以下省略形で書くことが推奨される
+        // 割り当てオペレータを使う事によって、
+        // 1,生じるエラーの可能性を避けることができる
+        // 2,そして正確性が増す
+        if (!isCar) {
+            System.out.println("This is not supposed to happen.");
+        }
     }
  }
 
