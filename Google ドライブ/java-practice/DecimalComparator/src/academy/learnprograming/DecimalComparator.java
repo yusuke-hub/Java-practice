57b07bd9dffe00c7bbe149b6f1d4ad2ff7868a98
yusuke-hub <yk1210140408@gmail.com>
2020-05-20 18:02:54 +0900

    Coding5(my solution to a problem)

---
+++ Google ドライブ/java-practice/DecimalComparator/src/academy/learnprograming/DecimalComparator.java
@@ -0,0 +1,40 @@
+package academy.learnprograming;
+
+public class DecimalComparator {
+    public static boolean areEqualByThreeDecimalPlaces(double d1, double d2){
+        // 小数点以下3桁以上、同じ数字であればtrue
+        // そうでなければfalseを返す
+        // d1, d2 < 0, Math.round(d1 - d2) = 0 true
+        // d1, d2 > 0, d1 - d2 != 0, d2 - d1 != 0 false
+        // d1 = d2, true
+        // Math.round(d1 + d2) = 0
+        int roundMinusSum = (int) Math.round(d1 - d2);
+        int roundPlusSum = (int) Math.round(d1 + d2);
+
+        if (d1 == d2) {
+            System.out.println("equal");
+            return true;
+        } else if((d1 > 0 && d2 > 0) && (d1 - d2 != 0 || d2 - d1 != 0)){
+            System.out.println("??");
+            if(d1 - d2 < 0.009 || d2 - d1 < 0.009){
+                return true;
+            } else if(d1 - d2 > 0.01 || d2 - d1 < 0.09){
+                return false;
+            }
+            return true;
+        } else if(roundMinusSum == 0){
+            if (d1 < 0 && d2 < 0 || d1 > 0 && d2 > 0) {
+                return true;
+            }
+            System.out.println("roundMinusSum");
+        } else if (roundPlusSum == 0){
+            if(d1 == 0 && d2 == 0){
+                return true;
+            }
+            System.out.println("roundPlusSum");
+            return false;
+        }
+        System.out.println("None");
+        return false;
+    }
+}
---
+++ Google ドライブ/java-practice/DecimalComparator/src/academy/learnprograming/Main.java
@@ -0,0 +1,8 @@
+package academy.learnprograming;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(DecimalComparator.areEqualByThreeDecimalPlaces(3.176, 3.175));
+    }
+}
