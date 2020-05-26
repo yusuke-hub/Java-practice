020fba14e9998682df98b4432a634a0d7c995409
yusuke-hub <yk1210140408@gmail.com>
2020-05-26 10:09:17 +0900

    Coding10

---
+++ Google ドライブ/java-practice/IntEqualityPrinter/src/academy/learnprograming/Main.java
@@ -0,0 +1,10 @@
+package academy.learnprograming;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    IntEqualityPrinter.printEqual(1, 1, 1);
+	    IntEqualityPrinter.printEqual(1, 1, 2);
+	    IntEqualityPrinter.printEqual(-1, -1, -1);
+    }
+}
