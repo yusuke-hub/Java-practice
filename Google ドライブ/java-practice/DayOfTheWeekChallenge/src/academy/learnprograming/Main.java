85144e22c0c8626618d3396bf5aa187565fcdb58
yusuke-hub <yk1210140408@gmail.com>
2020-05-26 15:15:14 +0900

    5/63, Coding12

---
+++ Google ドライブ/java-practice/DayOfTheWeekChallenge/src/academy/learnprograming/Main.java
@@ -0,0 +1,61 @@
+package academy.learnprograming;
+
+import java.time.DayOfWeek;
+
+public class Main {
+
+    public static void main(String[] args) {
+        printDayOfTheWeek(2);
+        printDayOfTheWeek2(5);
+    }
+    public static void printDayOfTheWeek(int day){
+        System.out.println("Switch Statement");
+        switch(day){
+            case 0:
+                System.out.println("Sunday");
+                break;
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            default:
+                System.out.println("Invalid day");
+                break;
+        }
+    }
+
+    public static void printDayOfTheWeek2(int day){
+        System.out.println("If Then Else Statement");
+        if(day == 0){
+            System.out.println("Sunday");
+        } else if(day == 1){
+            System.out.println("Monday");
+        } else if(day == 2){
+            System.out.println("Tuesday");
+        } else if(day == 3){
+            System.out.println("Wednesday");
+        } else if(day == 4){
+            System.out.println("Thursday");
+        } else if(day == 5){
+            System.out.println("Friday");
+        } else if(day == 6){
+            System.out.println("Saturday");
+        } else {
+            System.out.println("Invalid Value");
+        }
+    }
+}
