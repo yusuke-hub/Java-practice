d8fc4ffd20fc5e904067f43f7e9d3721f0edc691
yusuke-hub <yk1210140408@gmail.com>
2020-06-05 09:20:20 +0900

    Coding16

---
+++ Google ドライブ/java-practice/Coding16/src/academy/learnprograming/Main.java
@@ -0,0 +1,27 @@
+package academy.learnprograming;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(sumFirstAndLastDigit(5));
+    }
+
+    public static int sumFirstAndLastDigit(int number){
+        if(number < 0){
+            return -1;
+        } else if(number == 0){
+            return 0;
+        } else if (number < 10 && number != 0){
+            return number * 2;
+        }
+
+        int lastDigit = number % 10;
+        while(number > 0){
+            number /= 10;
+            if(number < 10 || number == 0){
+                break;
+            }
+        }
+        return lastDigit + number;
+    }
+}
