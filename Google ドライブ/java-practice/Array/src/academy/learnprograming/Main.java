4f11841641b39806f55b00fe4b20bed641efdb02
yusuke-hub <yk1210140408@gmail.com>
2020-06-20 10:00:17 +0900

    Defined methods to output a specified number of numbers on the command line or to return the average of the numbers entered.

--- Google ドライブ/java-practice/Array/src/academy/learnprograming/Main.java
+++ Google ドライブ/java-practice/Array/src/academy/learnprograming/Main.java
@@ -1,26 +1,37 @@
 package academy.learnprograming;
 
+import java.util.Scanner;
+
 public class Main {
 
+    private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-
-        int[] myIntArray = new int[25]; // {1,2,,3,4,5,6,7,8,9,10}
-//        myIntArray[0] = 45;
-//        myIntArray[1] = 476;
-//        myIntArray[5] = 50;
-
-        // myIntArrayの箇所を30にするとエラーになる
-        // 配列へのアクセスは存在する要素のみなので、要素が存在しない
-        // インデックスを指定するとエラーになる
-        for(int i=0; i<myIntArray.length; i++) {
-            myIntArray[i] = i*10;
+        int[] myIntegers = getIntegers(5);
+        for(int i=0; i<myIntegers.length; i++) {
+            System.out.println("Element " + i + ", typed value was " + myIntegers[i]);
         }
-        printArray(myIntArray);
+        System.out.println(getAverage(myIntegers));
     }
-    // 配列を引数にしたメソッドを定義する
-    public static void printArray(int[] array) {
-        for(int i=0; i<array.length; i++) {
-            System.out.println("Element " + i + ", value is " + array[i]);
+    // コマンドライン上で指定した数だけ、数字を入力して出力する
+    public static int[] getIntegers(int number) {
+        System.out.println("Enter " + number + " integer values.\r");
+        int[] values = new int[number];
+
+        for(int i= 0; i<values.length; i++) {
+            values[i] = scanner.nextInt();
         }
+
+        return values;
     }
+
+    // 入力した数の平均を取得する
+    public static double getAverage(int[] array) {
+        int sum = 0;
+        for(int i=0; i< array.length; i++) {
+            sum += array[i];
+        }
+        return (double) sum / (double)array.length;
+    }
+
 }
