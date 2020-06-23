e1e1e85cb2e9173670d1c524b8454205270f7002
yusuke-hub <yk1210140408@gmail.com>
2020-06-23 15:34:54 +0900

    文字列をチェックするメソッド、検索するメソッド

--- Google ドライブ/java-practice/A Practical Guide to Java/src/academy/learnprograming/Main.java
+++ Google ドライブ/java-practice/A Practical Guide to Java/src/academy/learnprograming/Main.java
@@ -3,6 +3,33 @@ package academy.learnprograming;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    String s1 = "スッキリJava";
+	    String s2 = "Java";
+	    String s3 = "java";
+	    String s4 = "";
+	    String s5 = "Java";
+	    if(s2.equals(s5)) {
+            System.out.println("s2とs5は等しい");
+        }
+        if(s2.equalsIgnoreCase(s3)) {
+            System.out.println("s2とs3はケースを区別しなければ等しい");
+        }
+        System.out.println("s1の長さは" + s1.length() + "です");
+        if(s4.isEmpty()) {
+            System.out.println("s4は空文字です");
+        }
+        System.out.println("");
+
+        String s6 = "Java and Javascript";
+        if(s6.contains("Java")) {
+            System.out.println("文字列s6は、Javaを含んでいます");
+        }
+        if(s6.endsWith("Java")) {
+            System.out.println("文字列s6は、Javaが末尾にあります");
+        }
+        System.out.println("文字列s6で最初にJavaが登場する位置は" + s6.indexOf("Java"));
+        System.out.println("文字列s6で最後にJavaが登場する位置は" + s6.lastIndexOf("Java"));
+
+
     }
 }
