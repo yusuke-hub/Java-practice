33ee5049af861bffb56c427a9aae1bebc5709522
yusuke-hub <yk1210140408@gmail.com>
2020-06-23 19:05:00 +0900

    LinkedHashSet/TreeSet

--- Google ドライブ/java-practice/A Practical Guide to Java/src/academy/learnprograming/Main.java
+++ Google ドライブ/java-practice/A Practical Guide to Java/src/academy/learnprograming/Main.java
@@ -1,24 +1,34 @@
 package academy.learnprograming;
 
-import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Main {
 
     public static void main(String[] args) {
-        // イテレータ
-            // リストに含まれる1つの箱を「ここ!」と 指している矢印のようなもの
-        ArrayList<String> names = new ArrayList<String>();
-        names.add("佐藤");
-        names.add("岸野");
-        names.add("嶋田");
-        // イテレータのインスタンスを生み出す場合は、newを使わず、
-        // igiteratorメソッドを呼び出す
-        // これによりリストの先頭(最初の箱よりもっと前）を指した状態の矢印を取得する
-        Iterator<String> it = names.iterator();
-        while(it.hasNext()) {
-            String e = it.next();
-            System.out.println(e);
+        // LinkedHashSet
+            // 値を格納した順序に整列するが、for文やイテレータを使って、セットの中身を1つずつ取り出す場合、
+            // どのような順序で要素が取り出せるかは一切確約されていない
+        Set<String> colors = new HashSet<String>();
+        colors.add("赤");
+        colors.add("青");
+        colors.add("黄");
+        colors.add("赤");
+        System.out.println("色は" + colors.size() + "種類"); // 色は3種類 → 重複は無視される
+        for(String s : colors) {
+            System.out.print(s + "→");
+        }
+        // TreeSet
+            // String型の複数の文字列を格納すると、「辞書順」で取り出すことができる
+        System.out.println("");
+        Set<String> words = new TreeSet<String>();
+        words.add("dog");
+        words.add("cat");
+        words.add("wolf");
+        words.add("panda");
+        for(String s : words) {
+            System.out.print(s + "→");
         }
     }
 
