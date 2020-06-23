ab9d534664752428d80456fe936d96386c35edcc
yusuke-hub <yk1210140408@gmail.com>
2020-06-23 19:33:08 +0900

    参照の落とし穴、注意点(コレクションに対する独自クラスの格納)

--- Google ドライブ/java-practice/A Practical Guide to Java/src/academy/learnprograming/Main.java
+++ Google ドライブ/java-practice/A Practical Guide to Java/src/academy/learnprograming/Main.java
@@ -2,23 +2,34 @@ package academy.learnprograming;
 
 import java.util.*;
 
+class Hero {
+    public String name;
+}
 public class Main {
-
     public static void main(String[] args) {
-        Map<String, Integer> prefs = new HashMap<String, Integer>();
-        prefs.put("京都府", 255);
-        prefs.put("東京都", 1261);
-        prefs.put("熊本県", 181);
-        int tokyo = prefs.get("東京都");
-//        System.out.println("東京都の人口は、" + tokyo);
-//        prefs.remove("京都府");
-        prefs.put("熊本県", 182);
-        int kumamoto = prefs.get("熊本県");
-//        System.out.println("熊本県の人口は、" + kumamoto);
-        for(String key : prefs.keySet()) {
-            int value = prefs.get(key);
-            System.out.println(key + "の人口は、" + value);
-        }
+        // ★参照の落とし穴
+            // 変数hやリストに格納された値が実際には参照であり、
+            // Heroインスタンスが生まれた時点で、そのメモリ上の番地がhに代入されている
+            // コレクションへ格納が終わった変数のインスタンスの中身を書き換えると、
+            // コレクションに格納済みの要素の中身も書き変わってしまう
+
+            //*注意点
+                // Heroのように自分で作るクラスをコレクションに格納する場合、とても重要なことがある
+                    // equals()/hashCode()を正しくオーバーライドしていないと、
+                    // コレクションは正しく動作せず、原因の特定が困難な不具合につながることがある
+                // 古参のコレクションクラス、VectorやHashableは
+                // ArrayListやHashMapに劣るので、特別な事情がない限り使用しない
+        // CollectionsクラスとArraysクラス
+            // java.util.Collections: コレクション操作関連の便利なメソッド集
+            // java.util.Arrays: 配列操作関連の便利なメソッド集
+        
+        Hero h = new Hero();
+        h.name = "ミナト";
+        List<Hero> list = new ArrayList<Hero>();
+        list.add(h);
+        h.name = "スガワラ";
+        System.out.println(list.get(0).name);
+        System.out.println(list.hashCode()); // 1513712059;
     }
 
 
