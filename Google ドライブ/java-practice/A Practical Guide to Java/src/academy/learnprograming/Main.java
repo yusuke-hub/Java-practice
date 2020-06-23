5fe86ef172374c81e66e0ee57cad2300d0ed4b2d
yusuke-hub <yk1210140408@gmail.com>
2020-06-23 19:15:33 +0900

    HashMap

--- Google ドライブ/java-practice/A Practical Guide to Java/src/academy/learnprograming/Main.java
+++ Google ドライブ/java-practice/A Practical Guide to Java/src/academy/learnprograming/Main.java
@@ -1,34 +1,23 @@
 package academy.learnprograming;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        // LinkedHashSet
-            // 値を格納した順序に整列するが、for文やイテレータを使って、セットの中身を1つずつ取り出す場合、
-            // どのような順序で要素が取り出せるかは一切確約されていない
-        Set<String> colors = new HashSet<String>();
-        colors.add("赤");
-        colors.add("青");
-        colors.add("黄");
-        colors.add("赤");
-        System.out.println("色は" + colors.size() + "種類"); // 色は3種類 → 重複は無視される
-        for(String s : colors) {
-            System.out.print(s + "→");
-        }
-        // TreeSet
-            // String型の複数の文字列を格納すると、「辞書順」で取り出すことができる
-        System.out.println("");
-        Set<String> words = new TreeSet<String>();
-        words.add("dog");
-        words.add("cat");
-        words.add("wolf");
-        words.add("panda");
-        for(String s : words) {
-            System.out.print(s + "→");
+        Map<String, Integer> prefs = new HashMap<String, Integer>();
+        prefs.put("京都府", 255);
+        prefs.put("東京都", 1261);
+        prefs.put("熊本県", 181);
+        int tokyo = prefs.get("東京都");
+//        System.out.println("東京都の人口は、" + tokyo);
+//        prefs.remove("京都府");
+        prefs.put("熊本県", 182);
+        int kumamoto = prefs.get("熊本県");
+//        System.out.println("熊本県の人口は、" + kumamoto);
+        for(String key : prefs.keySet()) {
+            int value = prefs.get(key);
+            System.out.println(key + "の人口は、" + value);
         }
     }
 
